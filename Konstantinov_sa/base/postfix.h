447409c1b49fb81b850098800e01b2b85ba3ead9
Projectius_student <sprojectiustech@gmail.com>
2024-11-05 19:00:51 +0300

    Операторы переделаны, вычисление реализовано (операторы пока только с 2 аргументами)

--- Konstantinov_sa/base/postfix.h
+++ Konstantinov_sa/base/postfix.h
@@ -1,78 +1,77 @@
 ﻿#pragma once
 
 #include <string>
+#include <vector>
 #include "stack.h"
-#include<functional>
-#include<unordered_map>
+#include <functional>
+#include <unordered_map>
 #include <sstream>
 #include <memory>
-#include<iostream>
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-enum LexemeType {lex, var, op, parOpen, parClose};
+enum LexemeType { lex, var, op, parOpen, parClose };
 
 class Lexeme
 {
-	bool defined = false;
-	string name;
-	LexemeType type = LexemeType::lex;
-
+    bool defined = false;
+    string name;
+    LexemeType type = LexemeType::lex;
 
 public:
-	Lexeme() { throw logic_error("Lexem default constructor should not be called. Invalid container usage."); } // Никогда не должен вызываться, может вызваться при неправильном использовании контейнеров и требуется для сборки
-	Lexeme(string name_) :name(name_) { cout << "HELLO :" << name_ << ":" << endl; 
-	};
-	Lexeme(string name_, bool defined) :name(name_),defined(defined) {};
-	explicit Lexeme(string name_, LexemeType type) :name(name_), type(type), defined(1) {}; //Только для специальных лексем, для которых важно только имя и тип - скобок
-	LexemeType getType() { return type; }
-	virtual int getPriority() const { return -1; } //Приоритет скобок -1
-	string getName() { return name; }
-	virtual void define() { defined = true; }
-	bool isDefined() { return defined; }
-	virtual ~Lexeme() {};
-	friend ostream& operator<<(ostream& out, Lexeme& lex);
+    Lexeme() { throw logic_error("Lexem default constructor should not be called. Invalid container usage."); } // Никогда не должен вызываться, может вызваться при неправильном использовании контейнеров и требуется для сборки
+    Lexeme(string name_) :name(name_) { cout << "HELLO :" << name_ << ":" << endl; };
+    Lexeme(string name_, bool defined) :name(name_), defined(defined) {};
+    explicit Lexeme(string name_, LexemeType type) :name(name_), type(type), defined(1) {}; // Только для специальных лексем, для которых важно только имя и тип - скобок
+    LexemeType getType() { return type; }
+    virtual int getPriority() const { return -1; } // Приоритет скобок -1
+    string getName() { return name; }
+    virtual void define() { defined = true; }
+    bool isDefined() { return defined; }
+    virtual ~Lexeme() {};
+    friend ostream& operator<<(ostream& out, Lexeme& lex);
 };
 
 ostream& operator<<(ostream& out, Lexeme& lex)
 {
-	cout<<" <<"<< lex.name<<endl;
-	return out << lex.name;
+    cout << " <<" << lex.name << endl;
+    return out << lex.name;
 }
 
 //////////////////////////////////////////
 template <class T>
 class Variable : public Lexeme
 {
-	T value;
+    T value;
 public:
-	Variable(string name) :Lexeme(name, LexemeType::var) {}
-	void define(T& value_) { value = value_; defined = true; }
-	T& getValue() { return value; }
+    Variable(string name) : Lexeme(name, LexemeType::var) {}
+    void define(T& value_) { value = value_; static_cast<Lexeme>(*this).define(); }
+    T& getValue() { return value; }
 };
 
 //////////////////////////////////////////
-template <typename Func>
+template <typename T>
 class Operator : public Lexeme
 {
-	int argCount;
-	int priority;
-	Func operation;
+    int argCount;
+    int priority;
+    std::function<T(T, T)> operation;
 public:
-	//explicit Operator(string name, int argCount, int priority = 0) : Lexeme(name), argCount(argCount), priority(priority) {}
-	explicit Operator(string name_, int argCount_, int priority_, Func&& operation_) : Lexeme(name_, LexemeType::op), argCount(argCount_), priority(priority_), operation(move(operation_)) { cout << "prior " << priority; }
-	//void define(Func&& operation_) { operation = move(operation_); defined = true; }
+    explicit Operator(string name_, int argCount_, int priority_, std::function<T(T, T)> operation_)
+        : Lexeme(name_, LexemeType::op), argCount(argCount_), priority(priority_), operation(move(operation_))
+    {
+        cout << "prior " << priority;
+    }
 
-	int getPriority() const override { cout << "PRIORITY" << endl; return priority; }
+    int getPriority() const override { return priority; }
+    int getArgCount() const { return argCount; }
 
-	template<typename... Args>
-	auto Execute(Args&&... args) const {
-		// forward сохраняет оригинальные характеристики аргументов (r-value или l-value)
-		return operation(forward<Args>(args)...);
-	}
-
+    T Execute(T a, T b) const {
+        return operation(a, b);
+    }
 };
-
 
 template <typename T>
 class LexBase
@@ -101,10 +100,8 @@ public:
         return it->second;
     }
 
-    template <typename Func>
-    Lexeme* addOperator(string name, int argCount, int priority, Func&& operation) {
-        using OperatorType = Operator<Func>;
-        OperatorType* nOp = new OperatorType(name, argCount, priority, forward<Func>(operation));
+    Lexeme* addOperator(string name, int argCount, int priority, std::function<T(T, T)> operation) {
+        Operator<T>* nOp = new Operator<T>(name, argCount, priority, move(operation));
         map.emplace(name, nOp);
 
         cout << "ADDED OPERATOR: " << nOp->getName() << " with priority " << nOp->getPriority() << endl;
@@ -119,20 +116,28 @@ public:
         return nullptr;
     }
 
-    void addLexeme(Lexeme& lex) { 
+    void addLexeme(Lexeme& lex) {
         Lexeme* nlex = new Lexeme(lex);
         map.emplace(lex.getName(), nlex);
         cout << "ADDED LEX :" << lex.getName() << ":" << endl;
+    }
+
+    vector<pair<string, Lexeme*>> getAllLexemes() {
+        vector<pair<string, Lexeme*>> lexemes;
+        for (const auto& pair : map) {
+            lexemes.emplace_back(pair.first, pair.second);
+        }
+        return lexemes;
     }
 };
 
 template<typename T>
 class TPostfix
 {
-	LexBase<T> base;
-	string infix;
-	vector<Lexeme*> postfix;
-	TStack<Lexeme*> opStack;
+    LexBase<T> base;
+    string infix;
+    vector<Lexeme*> postfix;
+    TStack<Lexeme*> opStack;
 
     void parseToPostfix() {
         istringstream iss(infix);
@@ -177,7 +182,7 @@ class TPostfix
                         postfix.push_back(opStack.get_top());
                         opStack.pop();
                     }
-                    if (!opStack.empty()) {
+                    if (!opStack.empty() && opStack.get_top()->getType() == parOpen) {
                         opStack.pop(); // Убираем открывающую скобку из стека
                     }
                 }
@@ -188,17 +193,23 @@ class TPostfix
             cout << "Stack size: " << opStack.get_size() << endl;
             cout << "Postfix size: " << postfix.size() << endl;
             if (!postfix.empty())
-                cout << "Last in postfix: " << postfix.back() << endl;
+                cout << "Last in postfix: " << postfix.back()->getName() << endl;
             ++t;
         }
 
         // Перемещаем оставшиеся операторы в постфикс
         while (!opStack.empty()) {
-            postfix.push_back(opStack.get_top());
-            cout << "END Popping " << *opStack.get_top() << " stack size " << endl;
-            opStack.pop();
+            if (opStack.get_top()->getType() == parOpen) {
+                opStack.pop(); // Удаляем открывающую скобку, если она осталась
+            }
+            else {
+                postfix.push_back(opStack.get_top());
+                cout << "END Popping " << *opStack.get_top() << " stack size " << endl;
+                opStack.pop();
+            }
         }
     }
+
 public:
 
     TPostfix(string infix_, bool importBasicOperators = true)
@@ -212,27 +223,74 @@ public:
 
             base.addLexeme(Lexeme("(", parOpen));
             base.addLexeme(Lexeme(")", parClose));
-
         }
         cout << "POSTFIX START" << endl;
         parseToPostfix();
         cout << "RESULT:" << endl;
         cout << GetPostfix() << endl;
-
     }
-	string GetInfix() { return infix; }
+
+    string GetInfix() { return infix; }
     string GetPostfix()
     {
         string res;
-        for (int i = 0; i < postfix.size();i++) {
+        for (int i = 0; i < postfix.size(); i++) {
             cout << "res " << postfix[i]->getName() << endl;
             res += postfix[i]->getName();
         }
         return res;
     }
-	string ToPostfix();
-	T Calculate(); // Ввод переменных, вычисление по постфиксной форме
+    string ToPostfix();
+
+    void addOperator(const string& name, int argCount, int priority, function<T(T, T)> operation) {
+        base.addOperator(name, argCount, priority, operation);
+    }
+
+    void setVariable(const string& name, T value) {
+        Lexeme* lex = base.getLexeme(name);
+        if (lex && lex->getType() == LexemeType::var) {
+            dynamic_cast<Variable<T>*>(lex)->define(value);
+        }
+        else {
+            cout << "Variable not found: " << name << endl;
+            throw logic_error("Variable not found: " + name);
+        }
+    }
+
+    void setVariables(const vector<pair<string, T>>& vars) {
+        for (const auto& var : vars) {
+            setVariable(var.first, var.second);
+        }
+    }
+
+    vector<pair<string, T*>> getUndefinedVars() {
+        vector<pair<string, T*>> undefinedVars;
+        for (const auto& lexemePair : base.getAllLexemes()) {
+            Lexeme* lexeme = lexemePair.second;
+            if (lexeme->getType() == LexemeType::var) {
+                Variable<T>* var = static_cast<Variable<T>*>(lexeme);
+                if (!var->isDefined()) {
+                    undefinedVars.push_back({ var->getName(), var });
+                }
+            }
+        }
+        return undefinedVars;
+    }
+
+    T Calculate()
+    {
+        TStack<T> calcStack;
+        for (auto lexeme : postfix) {
+            if (lexeme->getType() == LexemeType::var) {
+                calcStack.push(dynamic_cast<Variable<T>*>(lexeme)->getValue());
+            }
+            else if (lexeme->getType() == LexemeType::op) {
+                T op2 = calcStack.get_top(); calcStack.pop();
+                T op1 = calcStack.get_top(); calcStack.pop();
+                calcStack.push(dynamic_cast<Operator<T>*>(lexeme)->Execute(op1, op2));
+            }
+        }
+        return calcStack.get_top();
+    }
 };
-
-
 
--- Konstantinov_sa/base_test/test_main.h
+++ Konstantinov_sa/base_test/test_main.h
@@ -6,7 +6,8 @@ int main(int argc, char** argv)
 	::testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
 
-	TPostfix<double> pf("a + b * c");
-
+	TPostfix<double> pf("c * ( a + b )"); //"e / ( ( a + b ) * c + d )"
+	pf.setVariables({ {"a", 4.2},{"b", 1.0},{"c", 2.0}});
+	cout << "CALCULATED = "<<pf.Calculate()<<endl;
 	system("pause");
 }
